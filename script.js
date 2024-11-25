0de95de3ce55e40745b024cc97a258e2e1b70d10
Prathmesh1812 <139982211+Prathmesh1812@users.noreply.github.com>
2024-11-25 10:53:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function fun() {
+    var menu = document.getElementById("menuList");
+    if (menu.style.display === "none") {
+        menu.style.display = "block";
+    }
+    else {
+        menu.style.display = "none";
+    }
+}
